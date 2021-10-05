abe8a27057f0367935c3b64af8234ddce7813b92
Ekaterina Tarkhanova <katenokkt556@gmail.com>
2021-10-05 19:13:45 +0500

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 10000;
+        int pricePerMile = 20;
+        int bonusMiles = ticketPrice / pricePerMile;
+
+        System.out.println(bonusMiles);
+    }
+}
